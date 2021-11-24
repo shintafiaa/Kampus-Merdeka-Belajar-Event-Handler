8d94c61a85cab5e01c32243a1a5732dacfccc821
Shinta Roudlotul Hanafia <77773437+shintafiaa@users.noreply.github.com>
2021-11-25 06:41:01 +0700

    upload bookshelf

---
+++ 12 Bookshelf Apps/js/script.js
@@ -0,0 +1,8 @@
+//menampilkan artikel buku
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("inputBook");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addIncompleteBookList();
+  });
+});
