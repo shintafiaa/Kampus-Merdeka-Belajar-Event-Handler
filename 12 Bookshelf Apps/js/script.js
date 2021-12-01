6c21f9f71f19d95684d912f5fbc8847fc1260ff3
Shinta Roudlotul Hanafia <77773437+shintafiaa@users.noreply.github.com>
2021-12-01 10:37:09 +0700

    Delete 12 Bookshelf Apps directory

--- 12 Bookshelf Apps/js/script.js
+++
@@ -1,8 +0,0 @@
-//menampilkan artikel buku
-document.addEventListener("DOMContentLoaded", function () {
-  const submitForm = document.getElementById("inputBook");
-  submitForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addIncompleteBookList();
-  });
-});
